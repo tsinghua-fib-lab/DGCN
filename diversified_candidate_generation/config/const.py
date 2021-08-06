d42a24ddb3e5808d59da628047d59e96a7fe652a
S4rawBer2y <davy.morgan@aliyun.com>
2021-08-06 10:29:17 +0800

    Add files via upload

---
+++ diversified_candidate_generation/config/const.py
@@ -0,0 +1,18 @@
+#!/usr/local/anaconda3/envs/torch-1.0-py3/bin/python
+#coding=utf-8
+#pylint: disable=no-member
+#pylint: disable=no-name-in-module
+#pylint: disable=import-error
+
+
+coo_record = 'coo_record.npz'
+train_coo_record = 'train_coo_record.npz'
+val_coo_record = 'val_coo_record.npz'
+test_coo_record = 'test_coo_record.npz'
+
+popularity = 'popularity.npy'
+item_cate_feature = 'item_cate_feature.csv'
+edge_sim = 'train_coo_edge_sim.npz'
+train_coo_adj_graph = 'train_coo_adj_graph.npz'
+
+ckpt = 'ckpt/'
